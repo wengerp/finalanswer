b12c074edacb53720ca543e952f1c094deb3f062
wengerp <info@wepcom.ch>
2022-10-27 18:09:53 +0200

    Initial Commit

---
+++ FinalAnswer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinalAnswer
+//
+//  Created by Patrick Wenger on 27.10.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
